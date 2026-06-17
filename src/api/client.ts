592fca4d96816899af0e9ff936992879aa998a5c
toshmaina <mainajames16972@gmail.com>
2026-06-17 16:40:34 +0300

    fix: fixes the endpoint not been well loaded from the env file                     Add app icons to the app

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-export const API_BASE_URL =
-  "https://carlton-unimperial-ornately.ngrok-free.dev";
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
 
 const client = axios.create({
   baseURL: API_BASE_URL,
